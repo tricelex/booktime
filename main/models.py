6bd5ade2556cdc9d39c78c21176f2d6958a9bcef
Chuckz Okoye <tricelex@gmail.com>
2020-10-08 15:44:30 +0100

    Configuring user types and permissions through fixtures json file

--- main/models.py
+++ main/models.py
@@ -93,6 +93,22 @@ class User(AbstractUser):
 
     objects = UserManager()
 
+    @property
+    def is_employee(self):
+        return self.is_active and (
+            self.is_superuser
+            or self.is_staff
+            and self.groups.filter(name="Employees").exists()
+        )
+
+    @property
+    def is_dispatcher(self):
+        return self.is_active and (
+            self.is_superuser
+            or self.is_staff
+            and self.groups.filter(name="Dispatchers").exists()
+        )
+
 
 class Address(models.Model):
     SUPPORTED_COUNTRIES = (
